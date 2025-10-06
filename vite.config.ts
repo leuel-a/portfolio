db523b99154412dc27068fdc6c42580995688fe8
leuel <leuela1993@gmail.com>
2025-10-06 08:45:36 +0300

    add script to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-const REPOSITORY_NAME = 'portfolio';
+const REPOSITORY_NAME = '/portfolio';
 
 export default defineConfig({
     base: REPOSITORY_NAME,
