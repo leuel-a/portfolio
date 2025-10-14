23dffbfa282dcea380ac0b108a33c752bcd41b58
leuel <leuela1993@gmail.com>
2025-10-14 07:10:49 +0300

    revert project for clean react start

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-
-const REPOSITORY_NAME = '/portfolio';
-
-export default defineConfig({
-    base: REPOSITORY_NAME,
-    plugins: [tailwindcss()]
-})
